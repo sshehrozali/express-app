ed45f92e0580aa364730f5300b8b4dae224bfe99
Shehroz Ali <sshehroza13@gmail.com>
2021-05-19 20:08:28 +0500

    Added MongoDB database service

--- app.js
+++ app.js
@@ -1,19 +1,70 @@
 const express = require("express");
 const app = express();      // Initiate express app
 const path = require("path");
+const fs = require("fs");
+const mongoose = require('mongoose');
 const port = 80;        // Default port
 
 
 
-// Access template file -> views dir
+
+// Starting mongodb
+mongoose.connect('mongodb://localhost/sampleData', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("MongoDB is connected...");
+});
+
+// Creating schema
+const formSchema = new mongoose.Schema({
+    fullname: String,
+    email: String,
+    password: String
+  });
+
+// Creating Model
+const data = mongoose.model("data", formSchema);
+
+
+
+
+
+
+
+// Serving static HTML file (index.html)
+app.get("/", (req, res)=> {
+    res.status(200).sendFile(path.join(__dirname, 'static/index.html'));
+});
+
+
+
+
+// Express app config
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
 
-// Render file
-app.get("/demo", (req, res)=> {
-    res.status(200).render('template', { title: 'Express App', message: 'This is an Express app made using Nodejs and Express module',
-credit: 'Developed By Syed Shehroz Ali' })
-});
+app.post("/registeredSuccessfully", (req, res) => {
+
+    const renderEngineObj = {
+        "message": `Welcome, ${req.body.fullName}`,
+        "status": "You are registered!"
+    };
+
+    // Save User's Fullname, Email and Password in MongoDB
+    const IndividualUserData = new data({ fullname: `${req.body.fullName}`, email: `${req.body.email}`, password: `${req.body.password}` });
+    IndividualUserData.save()
+    
+    res.status(200).render("template", renderEngineObj);
+    console.log(req.body.fullName, req.body.email, req.body.password);
+})
+
+
+
+
+
 
 // Start server
 app.listen(port, ()=> {
